32b865a1a8e5370985a727002b3ab70a024b2361
penguinyzsh <penguinyzsh@outlook.com>
2026-03-26 21:47:26 +0800

    chore: bump version to 1.0.5 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.pysh.janus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
