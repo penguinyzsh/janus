e1e43207ee5ee15163aab53b0d46b4f8cf12f460
penguinyzsh <penguinyzsh@outlook.com>
2026-03-29 01:14:23 +0800

    chore: bump version to 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.pysh.janus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
